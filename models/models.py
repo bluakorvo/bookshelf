dc2f1df46fea815043334c375066d30029015a84
Fernando Gallegos <fernando.gallegos@mtnet.com.mx>
2020-02-17 10:05:31 -0700

    [CHG]bookshelf changin ISBN to isbn on models

--- models/models.py
+++ models/models.py
@@ -2,23 +2,29 @@
 
 from odoo import models, fields, api, exceptions
 
+import re
+
+
 
 class Book(models.Model):
     _name = 'bookshelf.book'
     _description = 'Books from BookShelf'
 
     name = fields.Char(string="Title", required=True)
-    ISBN = fields.Char(required=True)
+    isbn = fields.Char(required=True)
     criticize = fields.Text()
 
     author_id = fields.Many2one('bookshelf.author', index=True)
 
     _sql_constraints = [
         ('ISBN_unique',
-         'UNIQUE(ISBN)',
+         'UNIQUE(isbn)',
          "ERROR: El ISBN debe ser unico"),
     ]
 
+    @api.depends('isbn')
+    def _verify_ISBN_integrity(self):
+        if self.isbn
 
 class Author(models.Model):
     _name = 'bookshelf.author'
